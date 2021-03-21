37cb9dd7d65ff7577cf15fb2f293e405bda6b9be
Hedi Raza <hraza@mail.uni-mannheim.de>
2021-03-21 21:53:54 +0100

    #27

--- src/main/java/com/scrab5/core/game/Bag_of_Tiles.java
+++ src/main/java/com/scrab5/core/game/Bag_of_Tiles.java
@@ -1,5 +1,36 @@
 package com.scrab5.core.game;
 
 public class Bag_of_Tiles {
-
+  private ArrayList<Tile> tiles = new ArrayList<Tile>();
+  
+  //Constructor
+  public Bag_of_Tiles() {
+    
+  }
+  
+  //Methods
+  public void add(Tile t) {
+    this.tiles.add(t);
+  }
+  
+  public void remove(Tile t) {
+    this.tiles.remove(t);
+  }
+  
+  public void setTiles(ArrayList<Tile> tiles) {
+    this.tiles = tiles;
+  }
+  
+  public ArrayList<Tile> getTiles(){
+    return this.tiles;
+  }
+  
+  public int getSize() {
+    return this.tiles.size();
+  }
+  
+  public Card get(int index) {
+    return this.tiles.get(index);
+  }
+  
 }
