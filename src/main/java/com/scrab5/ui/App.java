354d5af9c06b29961c4947434f4a3a4c82e3e16c
Markus Johannes Herre <mherre@mail.uni-mannheim.de>
2021-03-26 11:06:40 +0100

    #32 Sharing files for Login Screen

--- src/main/java/com/scrab5/ui/App.java
+++ src/main/java/com/scrab5/ui/App.java
@@ -1,39 +1,40 @@
 package com.scrab5.ui;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+  private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    scene = new Scene(loadFXML("Sample"), 1360, 768);
+    stage.setScene(scene);
+    stage.setTitle("Scrabble - Group 5");
+    stage.setResizable(false);
+    stage.show();
+  }
 
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+  public static void setRoot(String fxml) throws IOException {
+    scene.setRoot(loadFXML(fxml));
+  }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource( fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+  private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    return fxmlLoader.load();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 
 
 }
---
+++ src/main/java/com/scrab5/ui/SampleController.java
@@ -0,0 +1,56 @@
+package com.scrab5.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class SampleController implements Initializable {
+
+  @FXML
+  private ImageView quitButton;
+  @FXML
+  private ImageView createButton;
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @FXML
+  private void lighten(MouseEvent event) {
+    String id = ((ImageView) event.getSource()).getId();
+
+    if (id.equals("quitButton")) {
+      quitButton.setOpacity(1);
+
+    } else if (id.equals("createButton")) {
+      createButton.setOpacity(1);
+    }
+  }
+
+  @FXML
+  private void close(MouseEvent event) {
+    Stage s = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+    s.close();
+  }
+
+  @FXML
+  private void darken(MouseEvent event) {
+    ImageView iv = ((ImageView) event.getSource());
+    iv.setOpacity(0);
+  }
+
+
+  @FXML
+  private void create(MouseEvent event) {
+    Stage s = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+    s.close();
+  }
+
+}
