a4459b7db29eff1b1d3348f24cd8cdb9816594c7
Laura Maria Engist <lengist@mail.uni-mannheim.de>
2021-03-29 17:46:37 +0200

    #37 created skeletal structure of methods to fill the different tables and created the preparedStatements for server and player.

---
+++ src/main/java/com/scrab5/util/database/FillDatabase.java
@@ -0,0 +1,63 @@
+package com.scrab5.util.database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class FillDatabase extends Database {
+  private PreparedStatement pstmPlayer, pstmServer, pstmDic;
+
+  /* closes Statement stream */
+  private void closeStatements() {}
+
+  /* deletes all entries from table "name" */
+  private void deleteTable(String name) {}
+
+  /* creates the preparedStatements to insert something in a table */
+  private void prepareStatements() {
+    try {
+      pstmPlayer = connection.prepareStatement(
+          "INSERT INTO Player " + "(Name, Picture, TotalPoints, PersonalHighscore, LaidWords, "
+              + "PointsPerWordRate, LongestWord, TotalPlayedGames, TotalWins, "
+              + "WinRate, FaveDic) VALUES (?,?,?,?,?,?,?,?,?,?,?);");
+      pstmServer = connection.prepareStatement("INSERT INTO Server (ServerNames, Dictionaries,"
+          + "VictoryRanking, GameRanking, VictoryLossRate) VALUE (?,?,?,?,?);");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /*
+   * method to fill table player at specific index/column. If variable for column name is from type
+   * integer, variable content is default.
+   */
+  public void fillPlayer(int index, String name, String content, int content2) {}
+
+  /*
+   * method to fill table player completely. Used when a new player profile is created. Variables
+   * for statistics get default values.
+   */
+  public void fillPlayerComplete(String name, String picture) {}
+
+  /*
+   * method to fill table server at specific index/column.
+   */
+  public void fillServer(int index, String name, String content) {}
+
+  /*
+   * method to fill table server completely. Used when a new server is created. Variables for
+   * statistics get default values.
+   */
+  public void fillServerComplete(String name) {}
+
+  /* method to fill table dictionary from file */
+  public void fillDictionary(String name) {
+    // String filename: path to file in resources
+    String filename = null;
+    // create CSVReader for it?
+  }
+
+  /*
+   * for testing: public static void main(String[] args) { new FillDatabase().fillPlayer(…); }
+   */
+
+}
