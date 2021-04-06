25ec2dd710052e0eb05cdbe39dec23f3a24bd3d3
Markus Johannes Herre <mherre@mail.uni-mannheim.de>
2021-04-06 13:11:13 +0200

    #35 Small changes to textfield

--- src/main/java/com/scrab5/ui/AccountCreationController.java
+++ src/main/java/com/scrab5/ui/AccountCreationController.java
@@ -5,13 +5,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
-/**
- * @author marku
- * 
- */
+
 public class AccountCreationController extends Controller implements Initializable {
+
+	@FXML
+	private TextField nickname;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -27,6 +29,25 @@ public class AccountCreationController extends Controller implements Initializab
 	@FXML
 	private void back(MouseEvent event) throws IOException {
 		App.setRoot("Login");
+	}
+
+	/**
+	 * @author marku
+	 * @param event
+	 * @throws IOException
+	 * 
+	 * 
+	 */
+	@FXML
+	private void enterPressed(KeyEvent event) throws IOException {
+		switch (event.getCode()) {
+		case ENTER:
+			if (nickname.getText().length() <= 12) {
+				System.out.println(nickname.getText());
+			} else {
+				System.out.println("Too Long");
+			}
+		}
 	}
 
 }
