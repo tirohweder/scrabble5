e16d13f4ea03b6fcb22e5eceb143335c2942c11e
Markus Johannes Herre <mherre@mail.uni-mannheim.de>
2021-04-15 13:12:24 +0200

    #55 Updating everything

--- src/main/java/com/scrab5/ui/ProfileController.java
+++ src/main/java/com/scrab5/ui/ProfileController.java
@@ -5,13 +5,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class ProfileController extends Controller implements Initializable {
 
+  @FXML
+  private Label nicknameLabel;
+
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
-    // TODO Auto-generated method stub
+
+    this.nicknameLabel.setText(Data.getCurrentUser());
 
   }
 
