c6097181042d72c8fddc1f5c362df15a31b3c336
Markus Johannes Herre <mherre@mail.uni-mannheim.de>
2021-04-27 17:33:59 +0200

    #68 fixing

--- src/main/java/com/scrab5/ui/SettingsController.java
+++ src/main/java/com/scrab5/ui/SettingsController.java
@@ -3,8 +3,6 @@ package com.scrab5.ui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
@@ -23,6 +21,7 @@ public class SettingsController extends Controller implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     // TODO Auto-generated method stub
+    App.setMusicVolume(0);
 
   }
 
@@ -42,27 +41,8 @@ public class SettingsController extends Controller implements Initializable {
 
   @FXML
   private void setMusic() {
-    this.sliderMusic.valueChangingProperty().addListener(new ChangeListener<Boolean>() {
-
-      @Override
-      public void changed(ObservableValue<? extends Boolean> observable, Boolean wasChanging,
-          Boolean changing) {
-        // TODO Auto-generated method stub
 
 
-        String valueString = String.format("%1$.3f", sliderMusic.getValue());
-
-        if (changing) {
-          // startLog.getItems().add(
-          // valueString
-          // );
-        } else {
-          // endLog.getItems().add(
-          // valueString
-          // );
-        }
-      }
-    });
   }
 
 
