b32d9c2c75f2ce5b7e4a83d0a5eb5188a622adb1
Laura Maria Engist <lengist@mail.uni-mannheim.de>
2021-04-27 21:03:04 +0200

    #42 organized test structure and tested new implementations.

---
+++ src/test/java/com/scrab5/util/database/ConnectTest.java
@@ -0,0 +1,18 @@
+package com.scrab5.util.database;
+
+import org.junit.jupiter.api.Test;
+
+class ConnectTest {
+
+  @Test
+  void testDatabase() {
+    Database db = new Database();
+  }
+
+  /*
+   * @Test void testReconnect() { fail("Not yet implemented"); }
+   * 
+   * @Test void testDatabaseExistance() { fail("Not yet implemented"); }
+   */
+
+}
---
+++ src/test/java/com/scrab5/util/database/UseTest.java
@@ -0,0 +1,53 @@
+package com.scrab5.util.database;
+
+class UseTest {
+
+  /**
+   * Tests the function to return all the letters with their corresponding points that are saved in
+   * the table Server in the database.
+   * 
+   * @author lengist
+   */
+  /*
+   * @Test void testViewLetters() { FillDatabase.insertLetters("A", 4);
+   * FillDatabase.insertLetters("B", 1); FillDatabase.insertLetters("C", 2); ResultSet rs =
+   * UseDatabase.viewLetters(); try { while (rs.next()) { System.out.println(rs.getString(1)); } }
+   * catch (SQLException e) { e.printStackTrace(); } }
+   * 
+   * /** Tests the function to check if the table Player is empty.
+   * 
+   * @author lengist
+   */
+  /*
+   * @Test void testTablePlayerIsEmpty() { CreateDatabase db = new CreateDatabase(); if
+   * (UseDatabase.tablePlayerIsEmpty()) { System.out.println("The table player is empty"); } else {
+   * System.out.println("The table player is not empty"); } }
+   * 
+   * /** Tests the function to return all players from the table Player in a ResultSet.
+   * 
+   * @author lengist
+   */
+  /*
+   * @Test void testGetAllPlayerRS() { FillDatabase.createPlayer("Alpha", null);
+   * FillDatabase.createPlayer("Beta", null);
+   * 
+   * ResultSet rs = UseDatabase.getAllPlayerRS(); try { while (rs.next()) {
+   * System.out.println(rs.getString(1)); } } catch (SQLException e) { e.printStackTrace(); } }
+   * 
+   * /** Tests the function to return all players from the table Player in a Observable List.
+   * 
+   * @author lengist
+   */
+  /*
+   * @Test void testGetAllPlayer() { fail("Not yet implemented"); FillDatabase.createPlayer("Gamma",
+   * null); FillDatabase.createPlayer("Delta", null); ObservableList<String> list =
+   * UseDatabase.getAllPlayer(); }
+   * 
+   * /*
+   * 
+   * @Test void testPlayerExists() { fail("Not yet implemented"); }
+   * 
+   * @Test void testServerExists() { fail("Not yet implemented"); }
+   */
+
+}
