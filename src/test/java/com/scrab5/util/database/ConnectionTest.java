d309e0ba713be6a58c44eac0816007b68093817a
Laura Maria Engist <lengist@mail.uni-mannheim.de>
2021-04-27 12:55:25 +0200

    #67 fixed test classes

--- src/test/java/com/scrab5/util/database/ConnectionTest.java
+++ src/test/java/com/scrab5/util/database/ConnectionTest.java
@@ -3,10 +3,9 @@ package com.scrab5.util.database;
 import org.junit.Test;
 
 /**
- * @author lengist
+ * Class to test the creation of the connection to the database.
  * 
- *         Class to test the creation of the connection to the database.
- *
+ * @author lengist
  */
 public class ConnectionTest {
 
