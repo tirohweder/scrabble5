7a9cbc10956363ccd265e74db6902feeb6698cbb
Timm Rohweder <trohwede@mail.uni-mannheim.de>
2021-05-18 15:11:33 +0200

    #85 fixed singleplayer

--- src/main/java/com/scrab5/core/game/GameSession.java
+++ src/main/java/com/scrab5/core/game/GameSession.java
@@ -3,6 +3,7 @@ package com.scrab5.core.game;
 import com.scrab5.core.player.Player;
 import com.scrab5.ui.Data;
 import com.scrab5.util.database.Database;
+import com.scrab5.util.database.FillDatabase;
 import com.scrab5.util.database.UseDatabase;
 import java.io.Serializable;
 import java.sql.ResultSet;
@@ -152,7 +153,7 @@ public class GameSession implements Serializable {
 
     System.out.println("Initialized Bag");
 
-    //FillDatabase.fillLetters();
+    FillDatabase.fillLetters();
     ResultSet rs = UseDatabase.viewLetters();
     while (rs.next()) {
       this.bag.add(new Tile(rs.getString("Letter"), rs.getInt("Points")));
