e9048093f014bbe48fdc2907dc4176f15dc2afb7
Laura Maria Engist <lengist@mail.uni-mannheim.de>
2021-05-27 10:32:08 +0200

    #91 fixed a test bug

--- src/test/java/com/scrab5/core/game/GameBoardTest.java
+++ src/test/java/com/scrab5/core/game/GameBoardTest.java
@@ -338,26 +338,27 @@ class GameBoardTest {
   @Test
   void checkWordsLegit() {
     DictionaryParser.setCurrentDictionary("Built-In Standard Dictionary");
+    DictionaryParser.parseFile("Built-In Standard Dictionary");
     gameBoardTest.placeTileTest(new Tile("H", 3), 0, 0);
     gameBoardTest.placeTileTest(new Tile("E", 3), 0, 1);
     gameBoardTest.placeTileTest(new Tile("L", 3), 0, 2);
     gameBoardTest.placeTileTest(new Tile("L", 3), 0, 3);
 
-    assertTrue(gameBoardTest.checkWordsLegit());
+    assertTrue(gameBoardTest.checkWordsLegitTest());
 
     gameBoardTest.placeTileTest(new Tile("W", 3), 2, 0);
     gameBoardTest.placeTileTest(new Tile("E", 3), 2, 1);
     gameBoardTest.placeTileTest(new Tile("L", 3), 2, 2);
     gameBoardTest.placeTileTest(new Tile("L", 3), 2, 3);
 
-    assertTrue(gameBoardTest.checkWordsLegit());
+    assertTrue(gameBoardTest.checkWordsLegitTest());
 
     gameBoardTest.placeTileTest(new Tile("X", 3), 4, 0);
     gameBoardTest.placeTileTest(new Tile("X", 3), 4, 1);
     gameBoardTest.placeTileTest(new Tile("X", 3), 4, 2);
     gameBoardTest.placeTileTest(new Tile("X", 3), 4, 3);
 
-    assertFalse(gameBoardTest.checkWordsLegit());
+    assertFalse(gameBoardTest.checkWordsLegitTest());
   }
 
   @Test
